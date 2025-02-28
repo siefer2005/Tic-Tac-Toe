a2a94eafd138d6bce3c64009930e1d18abce076e
seifer2005 <spidypotter2020@gmail.com>
2025-02-28 12:46:52 +0530

    intial commit

---
+++ app/src/main/java/com/helo/tictactoe/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.helo.tictactoe
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat.startActivity
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.helo.tictactoe.databinding.ActivityMainBinding
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createOfflineBtn.setOnClickListener {
+            createOfflineGame()
+        }
+
+        binding.createOnlineGameBtn.setOnClickListener {
+            createOnlineGame()
+        }
+
+        binding.joinOnlineGameBtn.setOnClickListener {
+            joinOnlineGame()
+        }
+
+    }
+
+
+    fun createOfflineGame(){
+        GameData.saveGameModel(
+            GameModel(
+                gameStatus = GameStatus.JOINED
+            )
+        )
+        startGame()
+    }
+
+    fun createOnlineGame(){
+        GameData.myID = "X"
+        GameData.saveGameModel(
+            GameModel(
+                gameStatus = GameStatus.CREATED,
+                gameId = Random.nextInt(1000..9999).toString()
+            )
+        )
+        startGame()
+    }
+
+    fun joinOnlineGame(){
+        var gameId = binding.gameIdInput.text.toString()
+        if(gameId.isEmpty()){
+            binding.gameIdInput.setError("Please enter game ID")
+            return
+        }
+        GameData.myID = "O"
+        Firebase.firestore.collection("games")
+            .document(gameId)
+            .get()
+            .addOnSuccessListener {
+                val model = it?.toObject(GameModel::class.java)
+                if(model==null){
+                    binding.gameIdInput.setError("Please enter valid game ID")
+                }else{
+                    model.gameStatus = GameStatus.JOINED
+                    GameData.saveGameModel(model)
+                    startGame()
+                }
+            }
+
+    }
+
+    fun startGame(){
+        startActivity(Intent(this,GameActivity::class.java))
+    }
+
+}
+
